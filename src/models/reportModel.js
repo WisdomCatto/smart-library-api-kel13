87987aa2e36df90c5ca83c828c319985d01267ec
User <bagusariqambiya@gmail.com>
2026-04-27 23:21:56 +0700

    final final api

--- src/models/reportModel.js
+++ src/models/reportModel.js
@@ -2,15 +2,14 @@ import { pool } from '../config/db.js';
 
 export const ReportModel = {
   async getStats() {
-    const query = `
+    const result = await pool.query(`
       SELECT
         (SELECT COUNT(*) FROM books) AS total_books,
         (SELECT COUNT(*) FROM authors) AS total_authors,
         (SELECT COUNT(*) FROM categories) AS total_categories,
         (SELECT COUNT(*) FROM loans WHERE status = 'BORROWED') AS total_borrowed
-    `;
+    `);
 
-    const result = await pool.query(query);
     return result.rows[0];
   }
 };
