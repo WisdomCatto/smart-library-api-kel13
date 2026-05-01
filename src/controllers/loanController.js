d3cbe4ad5f55c134269afb87b1f356eef4295b46
User <bagusariqambiya@gmail.com>
2026-05-01 19:24:16 +0700

    FORE REALSISES THISTIME

--- src/controllers/loanController.js
+++ src/controllers/loanController.js
@@ -66,6 +66,24 @@ async getTopBorrowers(req, res) {
     } catch (err) {
       res.status(500).json({ error: err.message });
    }
+},
+  async deleteLoan(req, res) {
+  try {
+    const { id } = req.params;
+
+    const result = await LoanModel.deleteLoan(id);
+
+    if (!result) {
+      return res.status(404).json({ error: "Data loan tidak ditemukan" });
+    }
+
+    res.json({
+      message: "Loan berhasil dihapus"
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
 
 };
